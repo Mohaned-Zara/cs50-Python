ea87ec6f4336c09decead3225972e51207573483
Mohaned-Zara <mohanedzare@gmail.com>
2025-03-20 05:58:21 +0200

    added the code

--- project-2/plates/plates.py
+++ project-2/plates/plates.py
@@ -6,8 +6,48 @@ def main():
         print("Invalid")
 
 
-#def is_valid(s):
+def is_valid(plate):
+    if len(plate) > 7:
+        return False
+    else:
+        
+        # check for Third digit not equal zero
+        if plate[2] == "0" or plate[3] == "0":
+            return False
+        
+        else:
+        
+            # check for first 3 characters 
+
+            for char in plate[0:2]:
+                if char.isalpha():
+                    continue
+                else:
+                    print(char)
+                    return False
+        
+            # check for last 3 characters
     
+            for num in plate[3:]:
+                if num.isdigit():
+                    continue
+                else:
+                    print(num)
+                    return False
+            return True
+
+
+    """
+    ltrs=""
+    nums=""
+    for i in s:
+        if i.isalpha():
+            ltrs = ltrs + i
+        elif i.isdigit():
+            nums += i
+    int_num=int(nums)
+    """
+
 
 
 main()
