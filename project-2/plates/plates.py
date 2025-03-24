bb8a93cf01b0706a91ed7f3d3e7abf2f383c7719
Mohaned-Zara <mohanedzare@gmail.com>
2025-03-24 05:48:21 +0200

    solved it but not worth it

--- project-2/plates/plates.py
+++ project-2/plates/plates.py
@@ -1,3 +1,4 @@
+
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
@@ -7,34 +8,68 @@ def main():
 
 
 def is_valid(plate):
-    if 2 <= len(plate) < 7:
-        return False
-    else:
-        
-        # check for Third digit not equal zero
-        if plate[2] == "0" or plate[3] == "0":
-            return False
-        
-        else:
-        
-            # check for first 3 characters 
 
-            for char in plate[0:2]:
-                if char.isalpha():
-                    continue
-                else:
-                    print(char)
+    if len(plate) < 2 or len(plate) > 7:
+        return False
+
+    else:
+        #check first ans second char is char
+        if plate[0].isalpha() == False or plate[1].isalpha() == False:
+            return False
+
+        for i in range(len(plate)):
+            if plate[i].isdigit():
+                if plate[i] == "0":
                     return False
-        
+                if plate[i:].isdigit() == False:
+                    return False
+                break
+
+        return True
+
+
+
+
+
+
+
+
+"""
+
+        if (plate[2].isalnum() == True and plate[2] == "0"):
+            return False
+
+
+            #check all char is char
+        for allchar in plate[:]:
+            if allchar.isalnum():
+                return True
+            else:
+                break
+
+
+            # check for first 2 characters
+        for char in plate[0:2]:
+            if char.isalpha():
+                continue
+            else:
+                print(char,"char")
+                return False
+
             # check for last 3 characters
-    
-            for num in plate[3:]:
-                if num.isdigit():
-                    continue
-                else:
-                    print(num)
-                    return False
-            return True
+        for num in plate[2:]:
+            if num.isdigit():
+                continue
+            else:
+                print(num)
+                return False
+
+        return True
+"""
+
 
 
 main()
+
+
+
