108da7d956e8ede2f25555f8464edf5292becfe7
Mohaned-Zara <mohanedzare@gmail.com>
2025-07-02 20:34:34 +0300

    added game code

---
+++ project-4/game/game.py
@@ -0,0 +1,34 @@
+import random
+
+def main():
+        
+    while True:
+        try: 
+            level = int(input("Level:"))
+            if level > 0:
+                break
+
+            while(level <= int(0)):
+                level = int(input("Level:"))
+        except ValueError:
+            pass
+
+    random_number = random.randint(1, level)
+
+    while True:
+        try: 
+            guess = int(input("guess:"))
+            if guess < random_number:
+                print("Too small!")
+            elif guess > random_number:
+                print("Too large!")
+            else:
+                print("Just right!")  
+                break  
+        except ValueError:
+            pass
+
+
+
+if __name__=="__main__":
+    main()
